44ba0bbe9fcc7ae19d21e13d7b5c6f723269b106
kimD0ngjun <chickentasty0112@gmail.com>
2025-01-03 03:57:08 +0900

    refactor: 테스트 조건 수정 - 스레드풀 대신 직접 스레드 생성 + 대기 로직

--- src/main/java/com/example/visualvm_test/issue/thread_pool_blocking/ThreadWaitingController.java
+++ src/main/java/com/example/visualvm_test/issue/thread_pool_blocking/ThreadWaitingController.java
@@ -6,19 +6,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/pool")
+@RequestMapping("/thread")
 @RequiredArgsConstructor
-public class BlockingThreadPoolController {
+public class ThreadWaitingController {
 
-    private final BlockingThreadPoolService blockingThreadPoolService;
+    private final ThreadWaitingPoolService threadWaitingPoolService;
 
     /**
-     * VisualVM Heap Dump 체킹 + RejectedExecutionException 발생 가능성
+     * VisualVM Heap Dump 체킹 + 스레드풀은 RejectedExecutionException으로 효율 예외 처리?
+     * -> 직접 스레드를 생성하고 대기 상태로 오래 유지시키면 생기는 악영향?
      * -> 스레드풀 크기 확장 + HTTP 요청 타임아웃 적용
      */
     @GetMapping("/test")
     public String testBlockingThreadPool() {
-        blockingThreadPoolService.processRequest();
+        for (int i = 0; i < 100; i++) {
+            threadWaitingPoolService.processRequest(200);
+        }
         return "스레드풀 블로킹 처리!";
     }
 }
---
+++ src/main/java/com/example/visualvm_test/issue/thread_pool_blocking/ThreadWaitingPoolService.java
@@ -0,0 +1,28 @@
+package com.example.visualvm_test.issue.thread_pool_blocking;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class ThreadWaitingPoolService {
+
+    public void processRequest(int threadCount) {
+        for (int i = 0; i < threadCount; i++) {
+            new Thread(() -> {
+                try {
+                    log.info("스레드 시작 - {}", Thread.currentThread().getName());
+                    // 스레드를 대기 상태로 두기
+                    synchronized (this) {
+                        wait(); // 계속 대기 상태로 두기
+                    }
+                    log.info("스레드 종료 - {}", Thread.currentThread().getName());
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    log.error("스레드 중단됨", e);
+                }
+            }).start();
+        }
+    }
+}
+
