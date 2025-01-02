5d8616310d817447f60b108802ee39f039d96829
kimD0ngjun <chickentasty0112@gmail.com>
2025-01-02 16:33:23 +0900

    feat: 지연 초기화 어노테이션 할당

--- src/main/java/com/example/visualvm_test/issue/slow_bean_init/SlowInitBean.java
+++ src/main/java/com/example/visualvm_test/issue/slow_bean_init/SlowInitBean.java
@@ -1,20 +1,22 @@
 package com.example.visualvm_test.issue.slow_bean_init;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 /**
- * 의도적인 빈 초기화 지연
+ * 의도적인 빈 초기화 지연 -> VisualVM Profiler 활용
  */
 @Slf4j
 @Component
+@Lazy // 스프링은 즉시 초기화가 디폴트지만, 얘는 지연 초기화 어노테이션
 public class SlowInitBean {
     public SlowInitBean() throws InterruptedException {
-        Thread.sleep(10_000); // 10초 지현
-        log.warn("SlowInitBean 생성자 초기화");;
+        Thread.sleep(10_000); // 10초 지연
+        log.warn("*** 느릿느릿 SlowInitBean 생성자 초기화 ***");
     }
 
-    /**
-     * VisualVM Profiler 활용으로 초기화 시간 분 + @Lazy 어노테이션 적용해보기
-     */
+    public void performTask() {
+        log.info("*** SlowInitBean 작업 수행 ***");
+    }
 }
