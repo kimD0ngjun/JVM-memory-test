5f97335d8b9fa00cfd6f352f009c440383cf0e9c
kimD0ngjun <chickentasty0112@gmail.com>
2025-01-02 23:58:53 +0900

    feat: 메모리 누수 컨트롤러 세팅

--- src/main/java/com/example/visualvm_test/issue/memory_leak/MemoryLeakController.java
+++ src/main/java/com/example/visualvm_test/issue/memory_leak/MemoryLeakController.java
@@ -1,30 +1,28 @@
 package com.example.visualvm_test.issue.memory_leak;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * HTTP 요청 후, 메모리 사용량 증가하며 GC 미회수 객체 발생
  */
 @RestController
 @RequestMapping("/leak")
+@RequiredArgsConstructor
 public class MemoryLeakController {
-    // 누수된 메모리가 담기는 곳
-    private final List<byte[]> memoryLeak = new ArrayList<>();
 
-    @GetMapping("/test")
-    public String testMemoryLeak() {
-        // 1MB 데이터가 계속 누적
-        memoryLeak.add(new byte[1024 * 1024]);
-        return "메모리 누수 발생!";
-    }
+    private final MemoryLeakService memoryLeakService;
 
     /**
      * VisualVM Heap Dump 분석 + JMeter 호출 처리
      * -> 가상 사용자수 조건이 과하면 OutOfMemoryException 발생 가능성
      */
+    @GetMapping("/leak")
+    public String testMemoryLeak() {
+        memoryLeakService.generateLeak();
+        return "메모리 누수 발생!";
+    }
 }
+
