7e221ec54ca928cd46ea37e8b3ce67fc20ffa452
kimD0ngjun <chickentasty0112@gmail.com>
2025-01-03 02:47:00 +0900

    feat: 스레드풀 블로킹 처리 컨트롤러 엔드포인트 지정

---
+++ src/main/java/com/example/visualvm_test/issue/thread_pool_blocking/BlockingThreadPoolController.java
@@ -0,0 +1,22 @@
+package com.example.visualvm_test.issue.thread_pool_blocking;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/pool")
+@RequiredArgsConstructor
+public class BlockingThreadPoolController {
+
+    private final BlockingThreadPoolService blockingThreadPoolService;
+
+    /**
+     * VisualVM Heap Dump 체킹 + RejectedExecutionException 발생 가능성
+     * -> 스레드풀 크기 확장 + HTTP 요청 타임아웃 적용
+     */
+    @GetMapping("/test")
+    public String testBlockingThreadPool() {
+        blockingThreadPoolService.processRequest();
+        return "스레드풀 블로킹 처리!";
+    }
+}
